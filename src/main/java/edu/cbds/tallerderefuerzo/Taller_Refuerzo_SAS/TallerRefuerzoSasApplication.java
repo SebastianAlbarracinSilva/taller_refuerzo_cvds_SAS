e1231cc61cd7d80919ba8de2005c1126424e9d17
Sebastián Albarracín Silva <sebastian.albarracin@mail.escuelaing.edu.co>
2025-09-29 18:57:32 -0500

    first commit

---
+++ src/main/java/edu/cbds/tallerderefuerzo/Taller_Refuerzo_SAS/TallerRefuerzoSasApplication.java
@@ -0,0 +1,13 @@
+package edu.cbds.tallerderefuerzo.Taller_Refuerzo_SAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerRefuerzoSasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerRefuerzoSasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/cbds/tallerderefuerzo/Taller_Refuerzo_SAS/TallerRefuerzoSasApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cbds.tallerderefuerzo.Taller_Refuerzo_SAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerRefuerzoSasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
